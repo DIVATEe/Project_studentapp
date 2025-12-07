dcbb7c648d230dcbf3d95c8e349945943842b946
Amit_Divate <adivate49@gmail.com>
2025-12-07 17:22:24 +0530

    added db table

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,6 +18,21 @@ resource "aws_instance" "studentapp" {
      docker compose up -d
      
      mysql -h ${aws_db_instance.studentapp_db.address} -u admin -p${var.studentapp_db_password}
+
+     create database  studentapp;
+     use studentapp;
+      CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,  
+	    student_name VARCHAR(100) NOT NULL,  
+	    student_addr VARCHAR(100) NOT NULL,   
+	    student_age VARCHAR(3) NOT NULL,      
+	    student_qual VARCHAR(20) NOT NULL,     
+	    student_percent VARCHAR(10) NOT NULL,   
+	    student_year_passed VARCHAR(10) NOT NULL,  
+	    PRIMARY KEY (student_id)  
+      );
+      show tables;
+      exit
+      
      EOT
 }
 
